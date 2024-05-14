502bd60d683ee8af54981189c099d0775a984157
Jacob Grimes <grime3ja@dukes.jmu.edu>
2024-05-14 14:52:39 -0400

    search params and favorite functionality for search team

--- scripts/script.js
+++ scripts/script.js
@@ -9,16 +9,29 @@ function openPage() {
         "bet" : "bet.html",
         "aboutMe" : "about-me.html",
     };
-
+    
     let page = pages[selection];
-
+    
     if (page) {
         window.location.href = page;
     }
 }
 
-async function searchTeamPressed() {
-    let team = document.getElementById("search-bar").value;
+function searchTeamPressed() {
+    let searchText = document.getElementById('search-bar');
+    location.href = "./search-team.html?search=" + searchText.value;
+    sessionStorage.setItem("q", searchText.value);
+    const q = sessionStorage.getItem("q");
+    const urlSPObj = new URLSearchParams();
+    urlSPObj.append("search", q);
+}
+
+function searchPlayerPressed() {
+    // placeholder for when the search button is pressed
+}
+
+async function getTeamData(searchTerm) {
+    let team = searchTerm;
     const url = 'https://api-american-football.p.rapidapi.com/teams?search=' + team + '&league=1&season=2023';
     const options = {
         method: 'GET',
@@ -27,11 +40,11 @@ async function searchTeamPressed() {
             'X-RapidAPI-Host': 'api-american-football.p.rapidapi.com'
         }
     };
-
+    
     try {
         const result = await fetch(url, options);
         const teamInfo = (await result.json()).response[0];
-
+    
         let info = [];
         info.push("Team Name: " + teamInfo.name);
         info.push("City: " + teamInfo.city);
@@ -52,19 +65,42 @@ async function searchTeamPressed() {
         let img = document.getElementById("img")
         img.src = teamInfo.logo;
         img.alt = info[0] + "Logo";
+    
+        let recents = localStorage.getItem("recentTeams");
+        recents = JSON.parse(recents);
+        if (!recents) {
+            recents = [];
+        }
+        recents.push(teamInfo.name);
+        localStorage.setItem("recentTeams", JSON.stringify(recents));
+    
+        document.getElementById("favorite-button").addEventListener("click", () => {
+            let favoriteTeams = localStorage.getItem("favoriteTeams");
+            favoriteTeams = JSON.parse(favoriteTeams);
+            if (!favoriteTeams) {
+                favoriteTeams = [];
+            }
+            favoriteTeams.push(teamInfo.name);
+            localStorage.setItem("favoriteTeams", JSON.stringify(favoriteTeams));
+        });
     } catch (error) {
         console.error(error);
     }
 }
 
-function searchPlayerPressed() {
-    // placeholder for when the search button is pressed
-}
-
-function favoritePressed() {
-    // placeholder for when the favorite button is pressed
-}
-
 function clearPressed() {
     // placeholder for when the clear button is pressed
 }
+
+window.onload = ("load", () => {
+    const currentURLSearch = window.location.search;
+    const decoded = new URLSearchParams(currentURLSearch);
+    if (decoded.has("search")) {
+        const items = decoded.getAll("search");
+        sessionStorage.setItem("q", JSON.stringify(items));
+        const q = sessionStorage.getItem("q");
+        const urlSPObj = new URLSearchParams();
+        urlSPObj.append("search", q);
+        getTeamData(JSON.parse(q)[0]);
+    }
+});
